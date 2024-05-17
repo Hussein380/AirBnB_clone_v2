daa019954c2e9186f0937560ec27767bdfa40ba0
Hussein380 <huznigarane@gmail.com>
2024-05-17 11:35:41 +0300

    corrected my code to fullfill pep8 recommendations

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-
-# import necessary modules
+"""
+    Start Flask application
+"""
 from flask import Flask
 
-# Create a Flask application instance
+""" Create a Flask application instance"""
 app = Flask(__name__)
 
-# Define a route for the root URL ('/') with the option strict_slashes=False
-@app.route('/', strict_slashes=False)
+""" Define a route for the root URL ('/') with t
+    he option strict_slashes=False """
 
+
+@app.route('/', strict_slashes=False)
 def handle_requests():
-    # Define a function to handle requests to the defined route
+    """ Define a function to handle requests to the defined route """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-    # start te Flask development server
+    """ start te Flask development server"""
     app.run(host='0.0.0.0', port=5000)

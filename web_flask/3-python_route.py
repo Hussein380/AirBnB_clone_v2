1a3473adc9993a06a39dc5e10ff7738b814c2db5
Hussein380 <huznigarane@gmail.com>
2024-05-18 17:57:16 +0300

    Add Flask routes for '/', '/hbnb', '/c/<text>', and '/python/<text>' with corresponding functions

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" import flask from module"""
+from flask import Flask
+
+""" Create an instance of flask class"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    """ displays Hello HNBN """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """ displays HBNB """
+    return "HBNB"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """ displays c text"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

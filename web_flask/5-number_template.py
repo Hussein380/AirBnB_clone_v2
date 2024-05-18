6059dce326970ca984f387228aa36eb942a260ca
Hussein380 <huznigarane@gmail.com>
2024-05-18 19:51:46 +0300

    added task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,5 +38,11 @@ def display_number(n):
     return "{:d} is a number".format(n)
 
 
+@app.route('/number_template/<n>', strict_slashes=False)
+def display_template(n):
+    """ display a html page only if n is an integer """
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

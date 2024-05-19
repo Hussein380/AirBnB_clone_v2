fab42172dea2071ae65019af5bc7f58ecbb97e2d
Hussein380 <huznigarane@gmail.com>
2024-05-19 12:33:44 +0300

    added state.py file

--- models/state.py
+++ models/state.py
@@ -6,38 +6,29 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
-    """ This is the class for State 
+    """ This is the class for State
     Atteributes:
-        name: input name
+        name: input name:
     """
     # Define table name for the State class
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                           backref="state")
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              backref="state")
+    else:
+        name = ""
 
-    @property
-    # Dfine cities property to retrieve cities associated with this state
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            # iterate through all instances
-            # Replaces dots with spaces
-            city = key.replace('.', ' ')
-            # tokenize the string
-            city = shlex.split(city)
-            if city[0] == 'City':
-                lista.append(var[key])
-        # ietrate through city instance
-        for elem in lista:
-            # check if city belongs  to this states
-            if (elem.state_id == self.id):
-                result.append(elem)
-        # Return the list of cities associated with this state
-        return result
+    if models.storage_t != "db":
+        @property
+        # Dfine cities property to retrieve cities associated with this state
+        def cities(self):
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
